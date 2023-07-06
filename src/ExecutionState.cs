e3c274faa3ef976e2323d0ac1958696516706e49
JanSharp <steinmeister36@gmail.com>
2023-07-06 20:02:01 +0200

    Add changelog validation

--- src/ExecutionState.cs
+++ src/ExecutionState.cs
@@ -9,6 +9,14 @@ namespace VPMPublish
         private bool didAbort;
         private string packageRoot;
 
+        private PackageJson? packageJson;
+        private SemVersion? version;
+        private string? wholeChangelog;
+        ///<summary>
+        ///Excludes the `## [x.x.x] - YYYY-MM-DD` line.
+        ///</summary>
+        private string? changelogEntry;
+
         public ExecutionState(string packageRoot)
         {
             this.packageRoot = packageRoot;
@@ -37,58 +45,15 @@ namespace VPMPublish
             return result;
         }
 
-        private PackageJson? packageJsonCache;
-        private PackageJson GetPackageJson()
-        {
-            if (packageJsonCache != null)
-                return packageJsonCache;
-
-            string packageJsonPath = Path.Combine(packageRoot, "package.json");
-            if (!File.Exists(packageJsonPath))
-                throw Abort(new FileNotFoundException(
-                    "The package.json file should be directly inside the 'package-root'.",
-                    packageJsonPath
-                ));
-
-            using var fileStream = File.OpenRead(packageJsonPath);
-            // Can't use MayAbort with async functions. At least I don't know how to.
-            didAbort = true;
-            // For some reason DeserializeAsync is aborting the entire application
-            // without any sort of error or exception...
-            packageJsonCache = JsonSerializer.Deserialize<PackageJson>(fileStream);
-            didAbort = false;
-
-            if (packageJsonCache == null)
-                throw Abort("Invalid package.json... I don't have an error message to pass along.");
-
-            return packageJsonCache;
-        }
-
-        private string? changelogCache;
-        private string GetChangelog()
-        {
-            if (changelogCache != null)
-                return changelogCache;
-
-            string changelogPath = Path.Combine(packageRoot, "CHANGELOG.md");
-            if (!File.Exists(changelogPath))
-                throw Abort(new FileNotFoundException(
-                    "The CHANGELOG.md file should be directly inside the 'package-root'. "
-                        + "Changelogs are usually optional, however this publish script requires one. "
-                        + "It also must follow the https://common-changelog.org format, "
-                        + "the only slight exception being how 'unreleased' changes are kept in there.",
-                    changelogPath
-                ));
-
-            changelogCache = File.ReadAllText(changelogPath);
-            return changelogCache;
-        }
-
         public async Task<int> Publish()
         {
             try
             {
-                await Validate();
+                LoadPackageJson();
+                ValidatePackageJson();
+                LoadChangelog();
+                ValidateChangelog();
+                await Task.Delay(0); // HACK: To make it stop complaining about async.v
             }
             catch (Exception e)
             {
@@ -101,6 +66,27 @@ namespace VPMPublish
             return 0;
         }
 
+        private void LoadPackageJson()
+        {
+            string packageJsonPath = Path.Combine(packageRoot, "package.json");
+            if (!File.Exists(packageJsonPath))
+                throw Abort(new FileNotFoundException(
+                    "The package.json file should be directly inside the 'package-root'.",
+                    packageJsonPath
+                ));
+
+            using var fileStream = File.OpenRead(packageJsonPath);
+            // Can't use MayAbort with async functions. At least I don't know how to.
+            didAbort = true;
+            // For some reason DeserializeAsync is aborting the entire application
+            // without any sort of error or exception...
+            packageJson = JsonSerializer.Deserialize<PackageJson>(fileStream);
+            didAbort = false;
+
+            if (packageJson == null)
+                throw Abort("Invalid package.json... I don't have an error message to pass along.");
+        }
+
         private static Regex packageUrlRegex = new Regex(
             @"^https://github\.com/(?<user>[^/]+)/(?<repo>[^/]+)/"
                 + @"releases/download/v(?<version>[^/]+)/(?<name>[^/]+)\.zip$", RegexOptions.Compiled);
@@ -109,13 +95,9 @@ namespace VPMPublish
             @"^https://github\.com/(?<user>[^/]+)/(?<repo>[^/]+)/"
                 + @"blob/v(?<version>[^/]+)/CHANGELOG\.md$", RegexOptions.Compiled);
 
-        public async Task Validate()
+        public void ValidatePackageJson()
         {
-            await Task.Delay(0); // HACK: To make it stop complaining about async.
-            PackageJson packageJson = GetPackageJson();
-            string changelog = GetChangelog();
-
-            string packageName = packageJson.Name;
+            string packageName = packageJson!.Name;
             string packageVersion = packageJson.Version;
 
             if (Path.GetFileName(packageRoot) != packageName)
@@ -123,7 +105,7 @@ namespace VPMPublish
                     + $"folder name ({Path.GetFileName(packageRoot)}) must match."
                 );
 
-            SemVersion version = MayAbort(
+            version = MayAbort(
                 () => SemVersion.Parse(packageVersion, SemVersionStyles.Strict)
             );
 
@@ -175,6 +157,78 @@ namespace VPMPublish
                 throw Abort($"The package.json \"url\" contains the github repo name \"{repoInUrl}\" "
                     + $"while the \"changelogUrl\" contains \"{repoInChangelogUrl}\", which is a mismatch."
                 );
+        }
+
+        private void LoadChangelog()
+        {
+            string changelogPath = Path.Combine(packageRoot, "CHANGELOG.md");
+            if (!File.Exists(changelogPath))
+                throw Abort(new FileNotFoundException(
+                    "The CHANGELOG.md file should be directly inside the 'package-root'. "
+                        + "Changelogs are usually optional, however this publish script requires one. "
+                        + "It also must follow the https://common-changelog.org format, "
+                        + "the only slight exception being how 'unreleased' changes are kept in there.",
+                    changelogPath
+                ));
+
+            wholeChangelog = File.ReadAllText(changelogPath);
+        }
+
+        private static Regex changelogEntryRegex = new Regex(@"
+            ^\#\ Changelog
+            (?:\r\n|\r|\n){2} # Disallow extra blank lines. Only 1 is accepted.
+            \#\#\ \[(?<version>[^\]]+)\]\ -\ (?<date>[^\r\n]+) # The date gets validated later.
+            (?:\r\n|\r|\n){2} # Again, disallow extra blanks.
+            (?=[^\n\r]) # Except the next part starts with an optional newline match, which should be disallowed for the first match.
+            (?<entry>
+                (?:
+                    (?:\r\n|\r|\n)? # Only optional because the first line won't have a leading newline.
+                    (?!(?:\r\n|\r|\n)(?:\#\#\ |\[)) # Also not accepting a line starting with `[` as that is a link back ref.
+                    [^\r\n]* # * not + because empty lines are valid.
+                )+
+            )
+        ", RegexOptions.Compiled | RegexOptions.Multiline | RegexOptions.IgnorePatternWhitespace);
+
+        private static Regex dateRegex = new Regex(
+            @"[0-9]{4}-[0-9]{2}-[0-9]{2}",
+            RegexOptions.Compiled
+        );
+
+        private void ValidateChangelog()
+        {
+            Match entryMatch = changelogEntryRegex.Match(wholeChangelog!);
+
+            if (!entryMatch.Success)
+                throw Abort($"The changelog is malformed, please refer to "
+                    + $"https://common-changelog.org and verify your changelog."
+                );
+
+            if (packageJson!.Version != entryMatch.Groups["version"].Value)
+                throw Abort($"The version of the top entry in the changelog is '{entryMatch.Groups["version"].Value}' "
+                    + $"while the version in the package.json is '{packageJson.Version}', which is a mismatch. \n"
+                    + $"There's a good chance you forgot to update the changelog for this version, please refer to "
+                    + $"// TODO: insert link to documentation for generating the changelog entry here here."
+                );
+
+            string dateStr = entryMatch.Groups["date"].Value;
+            if (!dateRegex.IsMatch(dateStr))
+                throw Abort($"The date for the top entry in the changelog is '{dateStr}' "
+                    + $"which does not match the ISO-8601 format YYYY-MM-DD."
+                );
+
+            string expectedDateStr = DateTime.UtcNow.ToString("yyyy-MM-dd");
+            if (dateStr != expectedDateStr)
+                throw Abort($"The date for the top entry in the changelog is '{dateStr}' "
+                    + $"which does not match the expected date '{expectedDateStr}'. "
+                    + $"Chances are that you just generated the changelog entry a few minutes ago "
+                    + $"in which case this is a bit confusing, but the reason for that is the fact "
+                    + $"the changelog dates are in UTC, so the change from one day to the next "
+                    + $"most likely isn't at your midnight. \n"
+                    + $"If this is the case, simply update the date in the changelog, then run "
+                    + $"the commands 'git add CHANGELOG.md' and 'git commit --amend --no-edit'."
+                );
+
+            changelogEntry = entryMatch.Groups["entry"].Value;
         }
     }
 }
